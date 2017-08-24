f560a1a75d5fbb5a4d2e7a06a1ba18bbdfb7fbd0
llamacoder <30612433+llamacoder@users.noreply.github.com>
2017-08-24 08:39:23 -0600

    First working version
    HTML, CSS, jQuery, GitHub

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
   /* handle the clicks on the home and away game pics */
   $(".homeTeam").on("click", function() {
     pickHomeTeam($(this).closest(".game"));
@@ -7,38 +7,79 @@ $(document).ready(function () {
     pickAwayTeam($(this).closest(".game"));
   });
   $(".upArrow").on("click", function() {
-    alert("UpArrow click");
-    moveRowUp($(this).closest("game"));
+    moveRowUp($(this).closest(".game"));
   });
   $(".downArrow").on("click", function() {
-    alert("DownArrow click");
-    moveRowDown($(this).closest("game"));
+    moveRowDown($(this).closest(".game"));
   });
 });
 
 function pickHomeTeam(game) {
   var home = game.find(".homeTeam");
   var away = game.find(".awayTeam");
-    away.removeClass('selected');
-    away.addClass('unselected');
-    home.removeClass('unselected');
-    home.addClass('selected');
-};
+  away.removeClass('selected');
+  away.addClass('unselected');
+  home.removeClass('unselected');
+  home.addClass('selected');
+}
+
 function pickAwayTeam(game) {
   var home = game.find(".homeTeam");
   var away = game.find(".awayTeam");
-    home.removeClass('selected');
-    home.addClass('unselected');
-    away.removeClass('unselected');
-    away.addClass('selected');
-};
+  home.removeClass('selected');
+  home.addClass('unselected');
+  away.removeClass('unselected');
+  away.addClass('selected');
+}
 
 function moveRowUp(game) {
+  // get the row above this one
   var upperSibling = game.prev();
-  upperSibling.before("<p>test</p>");
-};
+
+  // update the points for both rows
+  var pts = game.find(".points");
+  pts.text(Number(pts.text()) + 1);
+  pts = upperSibling.find(".points");
+  pts.text(Number(pts.text()) - 1);
+
+  // switch the rows
+  upperSibling.before(game);
+
+  // update arrows
+  var arrow = upperSibling.find(".firstUpArrow");
+  if (arrow.length) {
+    arrow.removeClass("firstUpArrow");
+    game.find(".upArrow").addClass("firstUpArrow");
+  }
+  arrow = game.find(".lastDownArrow");
+  if (arrow.length) {
+    arrow.removeClass("lastDownArrow");
+    upperSibling.find(".downArrow").addClass("lastDownArrow");
+  }
+}
+
 function moveRowDown(game) {
+  // get the row below this one
   var lowerSibling = game.next();
-  game.remove();
+
+  // update the points for both rows
+  var pts = game.find(".points");
+  pts.text(Number(pts.text()) - 1);
+  pts = lowerSibling.find(".points");
+  pts.text(Number(pts.text()) + 1);
+
+  // switch the rows
   lowerSibling.after(game);
+
+  // update arrows
+  var arrow = lowerSibling.find(".lastDownArrow");
+  if (arrow.length) {
+    arrow.removeClass("lastDownArrow");
+    game.find(".downArrow").addClass("lastDownArrow");
+  }
+  arrow = game.find(".firstUpArrow");
+  if (arrow.length) {
+    arrow.removeClass("firstUpArrow");
+    lowerSibling.find(".upArrow").addClass("firstUpArrow");
+  }
 }
